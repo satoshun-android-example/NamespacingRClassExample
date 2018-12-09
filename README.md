b7c6837a9d6e23a27b7b35236f5e8f28b6cb94b1
SatoShun <shun.sato1@gmail.com>
2018-12-09 04:49:49 +0000

    fix README

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# DataBinding 3.4.0 alpha07 example
+# NameSpacing RClass example
--- base/src/main/java/com/github/satoshun/example/sample/module/Test.kt
+++ base/src/main/java/com/github/satoshun/example/sample/module/Test.kt
@@ -1,2 +1,7 @@
 package com.github.satoshun.example.sample.module
 
+import androidx.appcompat.R as AppCompatR
+
+fun test() {
+  AppCompatR.layout.abc_action_bar_title_item
+}
